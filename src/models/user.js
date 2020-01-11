dbae3393eeaa5afd90253b77759bb90851a6fd66
markleymsm <markley.mendes@gmail.com>
2020-01-11 13:25:54 -0300

    Primeiro commit

---
+++ src/models/user.js
@@ -0,0 +1,17 @@
+let mongoose = require('mongoose');
+
+let User = mongoose.Schema({
+    name: String,
+    email: {
+        type: String, 
+        unique: true
+    },
+    password: String,
+    accounts:[{
+        name: String,
+        role: String,
+        enabled: Boolean
+    }]
+});
+
+module.exports = mongoose.model('User', User);
